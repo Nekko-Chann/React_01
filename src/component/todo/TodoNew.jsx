ee06f2405bfa947817a45e19ede88f6505ea45fc
Neko-Chan <nekkochan2k@gmail.com>
2024-06-24 04:38:14 +0700

    Video 36

--- src/component/todo/TodoNew.jsx
+++ src/component/todo/TodoNew.jsx
@@ -2,11 +2,28 @@
 const TodoNew = (props) => {
     // console.log("check props: ", props);
     const { addNewTodo } = props;
-    addNewTodo("Edan");
+    // addNewTodo("Edan");
+
+    const handleClick = () => {
+        alert("Click me!")
+    }
+
+    const handleOnChange = (name) => {
+        console.log("HandleOnChange: ", name.target.value)
+    }
+
     return (
         <div className='todo-new'>
-            <input type="text" className="todo-input" placeholder='Enter your task' />
-            <button className='btn'>Add</button>
+            <input
+                type="text"
+                className="todo-input"
+                onChange={(event) => handleOnChange(event)}
+                placeholder='Enter your task' />
+            <button
+                className='btn'
+                onClick={handleClick}
+            >Add
+            </button>
         </div>
     );
 }
