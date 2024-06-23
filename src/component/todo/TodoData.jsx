95954d185672c45544753cf5d55b18d0b3a3a62d
Neko-Chan <nekkochan2k@gmail.com>
2024-06-23 08:30:20 +0700

    Video 34

--- src/component/todo/TodoData.jsx
+++ src/component/todo/TodoData.jsx
@@ -1,9 +1,9 @@
 
 const TodoData = (props) => {
-    console.log(">>> check props: ", props);
+    const { name, age, data } = props;
     return (
         <div className='todo-data'>
-            <div>My name is {props.name}</div>
+            <div>My name is {name}</div>
             <div>Learning React</div>
             <div>Watching Youtube</div>
         </div>
