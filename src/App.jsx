1efe594fe4cc4c1861ddb1624b9cb00dad47e812
Neko-Chan <nekkochan2k@gmail.com>
2024-06-24 23:20:19 +0700

    Video 51

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,18 @@
-
-import TodoData from './component/todo/TodoData'
-import TodoNew from './component/todo/TodoNew'
-import './component/todo/todo.css'
-import reactLogo from './assets/react.svg'
-import { useState } from 'react'
 import Header from './component/layout/header'
 import Footer from './component/layout/footer'
 import { Outlet } from 'react-router-dom'
 
+
+
 const App = () => {
-
-  const [todoList, setTodoList] = useState([])
-
-  const addNewTodo = (name) => {
-    const newTodo = {
-      id: randomIntFromInterval(1, 20000),
-      name: name
-    }
-    setTodoList([...todoList, newTodo])
-  }
-
-  const randomIntFromInterval = (min, max) => { // min and max included
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
-  const deleteTodo = (id) => {
-    const newTodo = todoList.filter(item => item.id !== id)
-    setTodoList(newTodo);
-  }
 
   return (
     <>
       <Header />
-      <div className="todo-container">
-        <div className="todo-title">Todo List</div>
-        <TodoNew addNewTodo={addNewTodo} />
-
-        {todoList.length > 0 ?
-          <TodoData
-            todoList={todoList}
-            deleteTodo={deleteTodo}
-          />
-          :
-          <div className='todo-image'>
-            <img src={reactLogo} className='logo' alt="" />
-          </div>
-        }
-      </div >
       <Outlet />
       <Footer />
     </>
   )
 }
 
-export default App
+export default App;
