fd001001a0f2ee8394449965de49877411758917
Neko-Chan <nekkochan2k@gmail.com>
2024-06-24 22:42:11 +0700

    Video 47

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import TodoNew from './component/todo/TodoNew'
 import './component/todo/todo.css'
 import reactLogo from './assets/react.svg'
 import { useState } from 'react'
+import Header from './component/layout/header'
+import Footer from './component/layout/footer'
 
 const App = () => {
 
@@ -27,22 +29,25 @@ const App = () => {
   }
 
   return (
-    <div className="todo-container">
-      <div className="todo-title">Todo List</div>
-      <TodoNew addNewTodo={addNewTodo} />
+    <>
+      <Header />
+      <div className="todo-container">
+        <div className="todo-title">Todo List</div>
+        <TodoNew addNewTodo={addNewTodo} />
 
-      {todoList.length > 0 ?
-        <TodoData
-          todoList={todoList}
-          deleteTodo={deleteTodo}
-        />
-        :
-        <div className='todo-image'>
-          <img src={reactLogo} className='logo' alt="" />
-        </div>
-      }
-
-    </div >
+        {todoList.length > 0 ?
+          <TodoData
+            todoList={todoList}
+            deleteTodo={deleteTodo}
+          />
+          :
+          <div className='todo-image'>
+            <img src={reactLogo} className='logo' alt="" />
+          </div>
+        }
+      </div >
+      <Footer />
+    </>
   )
 }
 
